138c8fbe79ef77ac0a0541c76637c9787db8c4b4
Nixia64 <marco.bon6415@gmail.com>
2025-06-11 15:29:40 +0200

    test

--- test/api.test.js
+++ test/api.test.js
@@ -1,58 +1,93 @@
-const chai = require('chai')
+const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { app, seedDatabase, userAccountService} = require("../main");   // TODO : remplacer par le nom de votre script principal
-const {expect} = require("chai");
+const { app, seedDatabase, userAccountService } = require("../main");
+const { expect } = require("chai");
 const should = chai.should();
 
 chai.use(chaiHttp);
 
 describe('API Tests', function() {
-    this.timeout(5000);
+    this.timeout(10000);
     let token = '';
 
-    // Connexion à l'API pour récupérer le token JWT
-    before( (done) => {
-        seedDatabase().then( async () => {
-            console.log("Creating test user");
-            userAccountService.insert('User1', 'user1', 'default').then( () => // TODO adapter à votre fonction insert()
-                chai.request(app)
-                    .post('/useraccount/authenticate') // TODO : remplacer par votre URL d'authentification
-                    .send({login: 'user1', password: 'default'}) // TODO : remplacer par les champs attendus par votre route
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        token = res.body.token;
-                        done();
-                    })
-        )})
+    // Création d'un utilisateur de test et récupération du token JWT
+    before((done) => {
+        seedDatabase().then(async () => {
+            // Supprime l'utilisateur s'il existe déjà
+            const existing = await userAccountService.get('user1');
+            if (existing) await userAccountService.dao.delete(existing.id);
+            await userAccountService.insert('User1', 'user1', 'default');
+            chai.request(app)
+                .post('/useraccount/authenticate')
+                .send({ login: 'user1', password: 'default' })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    token = res.body.token;
+                    done();
+                });
+        });
     });
 
     // Suppression de l'utilisateur utilisé à la fin des tests
-    after( (done) => {
-        console.log("Deleting test user")
+    after((done) => {
         userAccountService.get('user1').then(
             (user) => {
-                userAccountService.dao.delete(user.id).then(done())
+                if (user) {
+                    userAccountService.dao.delete(user.id).then(() => done());
+                } else {
+                    done();
+                }
             }
-        )
-    })
+        );
+    });
 
-    // Test avec un token JWT valide
-    it('should allow access with valid token', (done) => {
+    // Test IGDB genres (GET)
+    it('should fetch IGDB genres', (done) => {
         chai.request(app)
-            .get('/car') // TODO : remplacer par une de vos routes protégée par validateJWT
-            .set('Authorization', `Bearer ${token}`)
+            .get('/api/igdb/genres')
             .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('array'); // TODO : remplacer array par object si votre route retourne un objet
-                res.body.should.have.lengthOf(0); // TODO remplacer par votre test
+                res.body.should.be.a('array');
                 done();
             });
     });
 
-    // Test avec un token JWT non valide
-    it('should deny access with invalid token', (done) => {
+    // Test IGDB games (POST)
+    it('should fetch IGDB games', (done) => {
+        const body = `
+            fields name;
+            limit 1;
+        `;
         chai.request(app)
-            .get('/car') // TODO : remplacer par une de vos routes protégée par validateJWT
+            .post('/api/igdb/games')
+            .set('Content-Type', 'text/plain')
+            .send(body)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                if (res.body.length > 0) {
+                    res.body[0].should.have.property('name');
+                }
+                done();
+            });
+    });
+
+    // Test accès protégé avec token valide
+    it('should allow access to /api/library with valid token', (done) => {
+        chai.request(app)
+            .get('/api/library')
+            .set('Authorization', `Bearer ${token}`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
+    });
+
+    // Test accès protégé avec token invalide
+    it('should deny access to /api/library with invalid token', (done) => {
+        chai.request(app)
+            .get('/api/library')
             .set('Authorization', 'Bearer wrongtoken')
             .end((err, res) => {
                 res.should.have.status(401);
